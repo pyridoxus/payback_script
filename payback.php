a6b27099f73f3c483e6230dd8138c0a04f5545af
Craig McCulloch <cm@pyridoxus.com>
2012-09-03 07:45:01 -0500

    Intro page works.

--- payback.php
+++ payback.php
@@ -15,23 +15,37 @@ if (!$link) {
 }
 else
 { 
-echo 'Connected successfully'; 
-mysql_select_db(payback); 
-echo '<form action="payback.php" method="post">';
-echo '<input type="button" name="view" />View History<br />';
-echo '<input type="button" name="payment" />Make Payment<br />';
-echo '</form>';
+  echo 'Connected successfully'; 
+  mysql_select_db(payback); 
+  if(!$_POST["view"] and !$_POST["payment"])
+  {
+    echo '<form action="payback.php" method="post">';
+    echo '<input type="button" name="view" />View History<br />';
+    echo '<input type="button" name="payment" />Make Payment<br />';
+    echo '</form>';
+  }
+  if($_POST["view"])
+  {
+    echo 'Clicked view.';
+    echo '<form action="payback.php" method="post">';
+    echo '<input type="button" name="continue" />Continue<br />';
+    echo '</form>';
+  }
+  if($_POST["payment"])
+  {
+    echo 'Clicked payment.';
+    echo '<form action="payback.php" method="post">';
+    echo '<input type="button" name="continue" />Continue<br />';
+    echo '</form>';
+  }
+  mysql_close($link);
 }
 ?>  
-<form action="payback.php" method="post">
+<!--form action="payback.php" method="post">
 <input type="radio" name="name" value="Craig" /> Craig<br />
 <input type="radio" name="name" value="Melissa" /> Melissa<br />
 Amount: <input type="text" name="amount" />
 </form>
-
-$_POST["fname"];
-<?php
-mysql_close($link);
-?>
+-->
 </body>
 </html>
