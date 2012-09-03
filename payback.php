1066fd4c3140e2df26319d5b04e0e0a409896501
Craig McCulloch <cm@pyridoxus.com>
2012-09-03 09:05:20 -0500

    Able to view table and select to make a payment.

--- payback.php
+++ payback.php
@@ -6,7 +6,7 @@
 <title>Payback</title>
 </head>
 
-<body>
+<body><center>
 <h1>Payback Page</h1>
 <?php 
 $link = mysql_connect('pyridoxuscom.ipagemysql.com', 'pay', 'back'); 
@@ -33,19 +33,55 @@ while ($row = mysql_fetch_row($result)) {
 mysql_free_result($result);
   if(!isset($_POST['select']))
   {
-    echo '<form action="';
-    echo htmlentities($_SERVER['PHP_SELF']);
-    echo '" method="post">';
-    echo '<p><input type="radio" name="select" value="view" /> View History<br />';
-    echo '<input type="radio" name="select" value="payment" /> Make Payment<br /></p>';
-    echo '<input type="submit" name="submit" value="Go!"><br />';
-    echo '</form>';
+    if(isset($_POST['submit']))
+    {
+      echo '<h2>Payment Posted</h2>';
+      echo '<p><form action="';
+      echo htmlentities($_SERVER['PHP_SELF']);
+      echo '" method="post">';
+      echo '<input type="submit" name="done" value="Continue..." /><br />';
+      echo '</form></p>';
+    }
+    else
+    {
+      echo '<form action="';
+      echo htmlentities($_SERVER['PHP_SELF']);
+      echo '" method="post">';
+      echo '<p><input type="radio" name="select" value="view" /> View History<br />';
+      echo '<input type="radio" name="select" value="payment" /> Make Payment<br /></p>';
+      echo '<input type="submit" name="submit" value="Go!"><br />';
+      echo '</form>';
+    }
   }
   else
   {
     if($_POST["select"] == "view")
     {
-      echo 'Clicked view.<br />';
+      echo '<p><form action="';
+      echo htmlentities($_SERVER['PHP_SELF']);
+      echo '" method="post">';
+      echo '<input type="submit" name="done" value="Continue..." /><br />';
+      echo '</form></p>';
+      $sql = "SELECT * FROM money;";
+      $result = mysql_query($sql);
+      if (!$result)
+      {
+	echo "DB Error, could not get rows</br>";
+	echo 'MySQL Error: ' . mysql_error();
+	exit;
+      }
+      echo '<table border=1><tr><td>Date</td><td>Person</td><td>Amount</td><td>Running Total</td></tr>';
+      while ($row = mysql_fetch_row($result))
+      {
+	echo "<tr>";
+	for ($column = 1; $column < 5; $column++)
+	{
+	  echo "<td>{$row[$column]}</td>";
+	}
+	echo "</tr>";
+      }
+      echo "</table>";
+      mysql_free_result($result);
       echo '<p><form action="';
       echo htmlentities($_SERVER['PHP_SELF']);
       echo '" method="post">';
@@ -58,7 +94,10 @@ mysql_free_result($result);
       echo '<p><form action="';
       echo htmlentities($_SERVER['PHP_SELF']);
       echo '" method="post">';
-      echo '<input type="submit" name="done" value="Continue..." /><br />';
+      echo '<p><input type="radio" name="name" value="Craig" /> Craig<br />';
+      echo '<input type="radio" name="name" value="Melissa" /> Melissa<br /></p>';
+      echo '<p>Amount: <input type="text" name="amount" /></p>';
+      echo '<input type="submit" name="submit" value="Continue..." /><br />';
       echo '</form></p>';
     }
   }
@@ -71,5 +110,6 @@ mysql_free_result($result);
 Amount: <input type="text" name="amount" />
 </form>
 -->
+</center>
 </body>
 </html>
